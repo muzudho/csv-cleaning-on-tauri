c0ccb59f8d8008127fd299db3b3f4d2bc166cc65
muzudho <muzudho1@gmail.com>
2025-06-12 23:40:36 +0900

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,137 +1,128 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
-import { open } from '@tauri-apps/plugin-dialog';
+    import { ref } from "vue";
+    //import { invoke } from "@tauri-apps/api/core";
+    import { open } from '@tauri-apps/plugin-dialog';
 
-const dirPathVM = ref("C:\\Users\\muzud\\OneDrive\\ドキュメント\\temp");
-const itemsVM = ref<string[]>([]);  // 初期値は空のstring配列
+    const filePathVM = ref("C:\\Users\\muzud\\OneDrive\\ドキュメント\\temp\\temp.csv");
+    const selectedItemVM = ref()
+    const textVM = ref()
 
-// RustのResult<Vec<String>, String>に対応する型
-interface FileNamesAndResult {
-  Ok?: string[]; // 成功ケース
-  Err?: string;  // エラーケース
-}
+    async function onReadButtonClicked() {
+        console.log("［Read］ボタンを押したぜ。")
+        // Open a dialog
+        const filePath = await open({
+            multiple: false,
+            directory: false,  // ファイルを開く。
+            defaultPath: filePathVM.value
+        });
+        filePathVM.value = filePath ?? "";  // dirPath がヌルなら空文字列に変換。
+        textVM.value = "テスト中１ read"
+    }
 
-async function onOpenButtonClicked() {
-  console.log("［Open］ボタンを押したぜ。")
-  // Open a dialog
-  const dirPath = await open({
-    multiple: false,
-    directory: true,  // ディレクトリーを開く。
-    defaultPath: dirPathVM.value
-  });
-  dirPathVM.value = dirPath ?? "";  // dirPath がヌルなら空文字列に変換。
-  itemsVM.value = await fetchFileNames(); 
-}
+    async function onWriteButtonClicked() {
+        console.log("［Write］ボタンを押したぜ。")
+        textVM.value = "テスト中２ write"
+    }
 
-async function onRefreshButtonClicked() {
-  console.log("［Refresh］ボタンを押したぜ。")
-  itemsVM.value = await fetchFileNames();
-}
-
-// Tauriのコマンドを呼び出し。
-// 配列を返す。
-async function fetchFileNames(): Promise<string[]> {
-  const file_names_and_result = await invoke<FileNamesAndResult>('get_file_names', { dirPath: dirPathVM.value });
-  if (file_names_and_result.Ok) {
-    return file_names_and_result.Ok; // string[] を返す
-  }
-  console.error(file_names_and_result.Err);
-  return [];  // エラー時は空配列を返す
-}
+    async function onExecuteButtonClicked() {
+        console.log("［Execute］ボタンを押したぜ。")
+        textVM.value = "テスト中３ Execute"
+    }
 </script>
 
 <template>
-  <main class="container">
-    <div class="row">
-      <input style="width:80%; height: 10vh;" :value="dirPathVM">
-      <button @click="onOpenButtonClicked" style="width:20%; height: 10vh;">Open</button>
-    </div>
-    <div class="row">
-      <button @click="onRefreshButtonClicked" style="width:100%; height: 10vh;">Refresh</button>
-    </div>
-    <select style="width:100%; height:80vh;" size="5">
-      <option v-for="item in itemsVM" :key="item" :value="item">{{ item }}</option>
-    </select>
-  </main>
+    <main class="container">
+        <div class="row">
+            <input style="width:60%; height: 10vh;" :value="filePathVM">
+            <button @click="onReadButtonClicked" style="width:20%; height: 10vh;">Read</button>
+            <button @click="onWriteButtonClicked" style="width:20%; height: 10vh;">Write</button>
+        </div>
+        <div class="row">
+            <select v-model="selectedItemVM">
+                <option value="都道府県スプリット">都道府県スプリット</option>
+            </select>
+            <p>選ばれた値: {{ selectedItemVM || 'まだ選んでないよ！' }}</p>
+            <button @click="onExecuteButtonClicked" style="width:20%; height: 10vh;">Execute</button>
+        </div>
+        <textarea style="width:100%; height:80vh;" v-model="textVM"></textarea>
+    </main>
 </template>
 
 <style>
 :root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
 
-  color: #0f0f0f;
-  background-color: #f6f6f6;
+    color: #0f0f0f;
+    background-color: #f6f6f6;
 
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
 }
 
 .container {
-  height: 95vh;
-  margin: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
+    height: 95vh;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
 }
 
 .row {
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 }
 
 input,
 button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+    border: 1px solid transparent;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    color: #0f0f0f;
+    background-color: #ffffff;
+    transition: border-color 0.25s;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
 }
 
 button {
-  padding: 0.6em 1.2em;
-  cursor: pointer;
+    padding: 0.6em 1.2em;
+    cursor: pointer;
 }
 
 button:hover {
-  border-color: #396cd8;
+    border-color: #396cd8;
 }
 button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
+    border-color: #396cd8;
+    background-color: #e8e8e8;
 }
 
 input,
 button {
-  outline: none;
+    outline: none;
 }
 
 @media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
+    :root {
+        color: #f6f6f6;
+        background-color: #2f2f2f;
+    }
 
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
+    input,
+    button {
+        color: #ffffff;
+        background-color: #0f0f0f98;
+    }
+    button:active {
+        background-color: #0f0f0f69;
+    }
 }
-
 </style>
